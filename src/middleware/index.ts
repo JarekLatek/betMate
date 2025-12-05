338dfbeb2c4df4a4952a265696e2594554817727
Jarosław Latek <jaroslaw.latek@wavy-apps.com>
2025-12-05 14:45:18 +0100

    Change for new auth plan.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,18 @@
 import { defineMiddleware } from "astro:middleware";
 
-import { supabaseClient } from "../db/supabase.client.ts";
+import { createSupabaseServerClient } from "@/db/supabase.server";
 
-export const onRequest = defineMiddleware((context, next) => {
-  context.locals.supabase = supabaseClient;
+export const onRequest = defineMiddleware(async (context, next) => {
+  // Create a new Supabase client per-request with cookie access
+  context.locals.supabase = createSupabaseServerClient(context.cookies);
+
+  // Get session from cookies
+  const {
+    data: { session },
+  } = await context.locals.supabase.auth.getSession();
+
+  // Expose user data to context
+  context.locals.user = session?.user ?? null;
+
   return next();
 });

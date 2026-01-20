a37ab99532af46446c3487de3694621fc68f8ece
Jarosław Latek <jaroslaw.latek@wavy-apps.com>
2026-01-20 08:00:53 +0100

    Playwright config file update for tests.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
+
+// Załaduj zmienne środowiskowe z .env.test
+dotenv.config({ path: path.resolve(process.cwd(), ".env.test") });
 
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: "./tests/e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -19,7 +24,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
+    command: "npm run dev:e2e",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
